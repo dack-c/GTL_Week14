b48b5691d80ba10c8b931438ff8c72c281eeff9e
Donghee <luz0415@naver.com>
2025-11-22 00:20:39 +0900

    feat: ParticleSystemComponent 구현

--- Mundi/ParticleSystemComponent.cpp
+++ Mundi/ParticleSystemComponent.cpp
@@ -1,3 +1,60 @@
 ﻿#include "pch.h"
 #include "ParticleSystemComponent.h"
 
+UParticleSystemComponent::UParticleSystemComponent()
+{
+    // 여기에 임시 Template 만들어서 테스트 가능
+}
+
+UParticleSystemComponent::~UParticleSystemComponent()
+{
+    DestroyParticles();
+}
+
+void UParticleSystemComponent::InitParticles()
+{
+    DestroyParticles();
+    if (!Template) { return; }
+
+    // 에셋에 정의된 이미터 개수만큼 인스턴스 생성
+    for (int32 i = 0; i < Template->Emitters.Num(); i++)
+    {
+        UParticleEmitter* EmitterAsset = Template->Emitters[i];
+        
+        // 이미터가 유효하고 켜져있다면
+        if (EmitterAsset)
+        {
+            FParticleEmitterInstance* NewInst = new FParticleEmitterInstance();
+            NewInst->Init(EmitterAsset, this);
+            EmitterInstances.Add(NewInst);
+        }
+    }
+
+    if (bAutoActivate) { ActivateSystem(); }
+}
+
+void UParticleSystemComponent::TickComponent(float DeltaTime)
+{
+    Super::TickComponent(DeltaTime);
+
+    if (!bIsActive || !Template) return;
+
+    for (FParticleEmitterInstance* Inst : EmitterInstances)
+    {
+        Inst->Tick(DeltaTime);
+    }
+}
+
+void UParticleSystemComponent::DestroyParticles()
+{
+    for (FParticleEmitterInstance* Inst : EmitterInstances)
+    {
+        if (Inst)
+        {
+            Inst->FreeParticleMemory();
+            delete Inst;
+        }
+    }
+    EmitterInstances.Empty();
+    bIsActive = false;
+}
