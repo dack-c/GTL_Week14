1f02dacbcfcc2279706e78a2f4da5a0a71f892e5
Chanpago <jch8659@gmail.com>
2025-11-28 19:31:52 +0900

    feat: BodyInstance, ConstraintInstance 초기구축

---
+++ Mundi/Source/Runtime/Engine/Physics/BodyInstance.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+
+class UPrimitiveComponent;
+class UBodySetup;
+struct FPhysicsWorld;
+
+
+
+
+/**
+ * @brief 엔진에서 "물리 바디 한 개"를 대표하는 런타임 객체, 내부에 PxRigidDynamic / PxRigidStatic을 들고있고 OwnerComponent 포인터도 들고있다.
+ */
+
+struct FBodyInstance
+{
+    void InitDynamic(FPhysicsWorld& World, const FTransform& WorldTransform, float Mass);
+    void InitStatic(FPhysicsWorld& World, const FTransform& WorldTransform);
+
+    void Terminate(FPhysicsWorld& World);
+
+    void AddForce(const FVector& Force);
+    FTransform GetWorldTransform() const;
+
+
+public:
+    UPrimitiveComponent* OwnerComponent = nullptr;
+    UBodySetup*          BodySetup      = nullptr;
+    PxRigidActor*        RigidActor     = nullptr;
+};
---
+++ Mundi/Source/Runtime/Engine/Physics/ConstraintInstance.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "BodyInstance.h"
+
+
+
+struct FConstraintLimitData;
+
+struct FConstraintInstance
+{
+
+public:
+    void InitD6(FPhysicsWorld& World, const FTransform& ParentFrame, const FTransform& ChildFrame, const FConstraintLimitData& Limits);
+
+    void Terminate(FPhysicsWorld& World);
+    
+public:
+    FBodyInstance* ParentBody   = nullptr;
+    FBodyInstance* ChildBody    = nullptr;
+    PxJoint*       Joint        = nullptr;
+};
