206d9efdcbce09569044ea631c46b53e521ef40c
dack-c <qpxmaos3845@gmail.com>
2025-12-07 17:26:01 +0900

    feat: 구현 중

--- Mundi/Source/Runtime/Engine/Animation/AnimTypes.cpp
+++ Mundi/Source/Runtime/Engine/Animation/AnimTypes.cpp
@@ -3,9 +3,7 @@
 
 void IAnimPoseProvider::IgnoreRootBoneTransform(FTransform* OutRootPose, const FName& InRootBoneName, const UAnimDataModel* InDataModel)
 {
-	/*FTransform RootTransform = *OutRootPose;
-	RootTransform.Translation = InDataModel->EvaluateBoneTrackTransform(InRootBoneName, 0.0f, true).Translation;
-	*OutRootPose = RootTransform;*/
-	OutRootPose->Translation = FVector(0.0f, 0.0f, 0.0f);
-	OutRootPose->Rotation = FQuat::Identity();
+	const FTransform FirstFrameTransform = InDataModel->EvaluateBoneTrackTransform(InRootBoneName, 0.0f, true);
+	OutRootPose->Translation = FirstFrameTransform.Translation;
+	//OutRootPose->Rotation = FirstFrameTransform.Rotation;
 }
